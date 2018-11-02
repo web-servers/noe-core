f050a0a4733fc701b22d87fd53e7119f0fd64f07
Marek Czernek <mczernek@redhat.com>
2018-11-02 14:17:13 +0100

    Add JWS5 integration tests

---
+++ testsuite/src/test/groovy/noe/tomcat/BindingsTomcat9ConfiguratorIT.groovy
@@ -0,0 +1,19 @@
+package noe.tomcat
+
+import noe.common.utils.Java
+import noe.common.utils.Platform
+import org.junit.Assume
+import org.junit.BeforeClass
+
+class BindingsTomcat9ConfiguratorIT extends BindingsTomcatConfiguratorIT {
+
+  @BeforeClass
+  public static void beforeClass() {
+    Platform platform = new Platform()
+    Assume.assumeFalse("JWS is not supported on HP-UX => skipping", platform.isHP())
+    Assume.assumeTrue("Tomcat from JWS 5.0 requires at least Java 1.7", Java.isJdk1xOrHigher('1.8'))
+
+    loadTestProperties("/tomcat9-common-test.properties")
+    prepareWorkspace()
+  }
+}
---
+++ testsuite/src/test/groovy/noe/tomcat/JvmRouteTomcat9ConfiguratorIT.groovy
@@ -0,0 +1,20 @@
+package noe.tomcat
+
+import noe.common.utils.Java
+import noe.common.utils.Platform
+import org.junit.Assume
+import org.junit.BeforeClass
+
+class JvmRouteTomcat9ConfiguratorIT extends JvmRouteTomcatConfiguratorIT {
+
+  @BeforeClass
+  public static void beforeClass() {
+    Platform platform = new Platform()
+    Assume.assumeFalse("JWS is not supported on HP-UX => skipping", platform.isHP())
+    Assume.assumeTrue("Tomcat from JWS 3.0 requires at least Java 1.7", Java.isJdk1xOrHigher('1.8'))
+
+    loadTestProperties("/tomcat9-common-test.properties")
+    prepareWorkspace()
+  }
+
+}
