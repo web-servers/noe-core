044e8a1b347ee7846b2520bc8036aeb9e660d51a
Marek Czernek <mczernek@redhat.com>
2018-11-15 09:20:29 +0100

    Correct comments

--- testsuite/src/test/groovy/noe/tomcat/BindingsTomcat9ConfiguratorIT.groovy
+++ testsuite/src/test/groovy/noe/tomcat/BindingsTomcat9ConfiguratorIT.groovy
@@ -11,7 +11,7 @@ class BindingsTomcat9ConfiguratorIT extends BindingsTomcatConfiguratorIT {
   public static void beforeClass() {
     Platform platform = new Platform()
     Assume.assumeFalse("JWS is not supported on HP-UX => skipping", platform.isHP())
-    Assume.assumeTrue("Tomcat from JWS 5.0 requires at least Java 1.7", Java.isJdk1xOrHigher('1.8'))
+    Assume.assumeTrue("Tomcat from JWS 5.0 requires at least Java 1.8", Java.isJdk1xOrHigher('1.8'))
 
     loadTestProperties("/tomcat9-common-test.properties")
     prepareWorkspace()
--- testsuite/src/test/groovy/noe/tomcat/JmxTomcat9ConfiguratorIT.groovy
+++ testsuite/src/test/groovy/noe/tomcat/JmxTomcat9ConfiguratorIT.groovy
@@ -11,7 +11,7 @@ class JmxTomcat9ConfiguratorIT extends JmxTomcatConfiguratorIT {
   public static void beforeClass() {
     Platform platform = new Platform()
     Assume.assumeFalse("JWS is not supported on HP-UX => skipping", platform.isHP())
-    Assume.assumeTrue("Tomcat from JWS 3.0 requires at least Java 1.7", Java.isJdk1xOrHigher('1.8'))
+    Assume.assumeTrue("Tomcat from JWS requires at least Java 1.8", Java.isJdk1xOrHigher('1.8'))
 
     loadTestProperties("/tomcat9-common-test.properties")
     prepareWorkspace()
