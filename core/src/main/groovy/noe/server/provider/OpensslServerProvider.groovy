e53b55af11597feaaca4738b49c15facd18027dd
Jan Stourac <jstourac@redhat.com>
2019-01-15 08:40:03 +0100

    Workaround for JBCS-637--directories differ for platforms in jbcs

--- core/src/main/groovy/noe/server/provider/OpensslServerProvider.groovy
+++ core/src/main/groovy/noe/server/provider/OpensslServerProvider.groovy
@@ -1,5 +1,7 @@
 package noe.server.provider
 
+import noe.common.DefaultProperties
+import noe.common.utils.OpenSslVersion
 import noe.common.utils.Platform
 import noe.jbcs.utils.CoreServicesOpenSslHelper
 import noe.server.OpensslServer
@@ -15,10 +17,20 @@ class OpensslServerProvider {
     private static final Platform platform = new Platform()
 
     static OpensslServer coreServices(File basedir) {
-        File opensslBasedir = new File(basedir, CoreServicesOpenSslHelper.coreOpenSslDirName + File.separator + 'openssl')
-        return new OpensslServerBuilder(opensslBasedir)
-                .binDir(new File(opensslBasedir, platform.isRHEL() ? 'sbin' : 'bin'))
-                .build()
+        File openSSLBaseDir, openSSLBinDir
+
+
+        if (new OpenSslVersion("1.0.2n").equals(DefaultProperties.jbcsOpenSslVersion())
+                && (new Platform().isWindows() || new Platform().isSolaris())) {
+            // WORKAROUND of the issue https://issues.jboss.org/browse/JBCS-637
+            openSSLBaseDir = basedir
+            openSSLBinDir = new File(openSSLBaseDir, 'bin')
+        } else {
+            openSSLBaseDir = new File(basedir, CoreServicesOpenSslHelper.coreOpenSslDirName + File.separator + 'openssl')
+            openSSLBinDir = new File(openSSLBaseDir, platform.isRHEL() ? 'sbin' : 'bin')
+        }
+
+        return new OpensslServerBuilder(openSSLBaseDir).binDir(openSSLBinDir).build()
     }
 
     static OpensslServer 'default'() {
