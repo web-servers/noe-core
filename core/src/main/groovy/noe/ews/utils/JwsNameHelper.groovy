ddf938957364ddf56ca5f2d2f08be9401077dd2f
Marek Czernek <mczernek@redhat.com>
2019-01-22 09:47:58 +0100

    Add support for RHEL8 JWS and JBCS natives (#43)
    * Add support for RHEL8 installation
    
    * Add RHEL8 support for JBCS installer

--- core/src/main/groovy/noe/ews/utils/JwsNameHelper.groovy
+++ core/src/main/groovy/noe/ews/utils/JwsNameHelper.groovy
@@ -93,6 +93,7 @@ class JwsNameHelper {
       if (platform.isRHEL5()) result += 'RHEL5-'
       else if (platform.isRHEL6()) result += 'RHEL6-'
       else if (platform.isRHEL7()) result += 'RHEL7-'
+      else if (platform.isRHEL8()) result += 'RHEL8-'
       if (platform.isX86()) {
         result += "i386"
       } else if (platform.isX64()) {
