53f67e32267024198bd8f942e7db0828aed2c5ff
Marek Czernek <mczernek@redhat.com>
2019-02-22 09:12:12 +0100

    Fix the ApacheDS workspace (#53)
    * Implement destroy method for ApacheDS
    
    * Implement prepare method
    
    * Add newline to the

--- core/src/main/groovy/noe/workspace/WorkspaceApacheDS.groovy
+++ core/src/main/groovy/noe/workspace/WorkspaceApacheDS.groovy
@@ -1,13 +1,33 @@
 package noe.workspace
 
+import groovy.util.logging.Slf4j
+import noe.common.utils.JBFile
 import noe.ews.utils.ApacheDSUtils
 import noe.server.ApacheDS
 
+@Slf4j
 class WorkspaceApacheDS extends WorkspaceAbstract {
 
+  private static final String SERVER_ID = "ews-ldap-kerberos"
+  private static final String VERSION = "2M7"
+
   WorkspaceApacheDS() {
-    basedir = ApacheDSUtils.installApacheDS(new File(basedir.toString()))
-    serverController.addServer('ews-ldap-kerberos', ApacheDS.getInstance(basedir, '2M7'))
+    prepare()
   }
 
+  @Override
+  Object prepare() {
+    basedir = ApacheDSUtils.installApacheDS(new File(basedir.toString()))
+    serverController.addServer(SERVER_ID, ApacheDS.getInstance(basedir, VERSION))
+  }
+
+  @Override
+  Object destroy() {
+    serverController.removeServer(SERVER_ID)
+    File apacheDsBaseDir = new File(basedir)
+    if (apacheDsBaseDir.exists()) {
+      log.debug("Deleting $apacheDsBaseDir")
+      JBFile.delete(apacheDsBaseDir)
+    }
+  }
 }
