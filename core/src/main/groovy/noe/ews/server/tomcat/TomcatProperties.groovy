8ee2c31756c3cfaa6413aa64c68fc65224822d46
Paul Lodge <plodge@redhat.com>
2019-05-15 15:01:56 +0200

    Changed tomcat.major.version behaviour, now asserts if not set

--- core/src/main/groovy/noe/ews/server/tomcat/TomcatProperties.groovy
+++ core/src/main/groovy/noe/ews/server/tomcat/TomcatProperties.groovy
@@ -7,9 +7,17 @@ import noe.common.utils.Library
 /**
  *
  * @author Michal Karm Babacek <mbabacek@redhat.com>
+ * @author Paul Lodge <plodge@redhat.com>
  *
  */
 class TomcatProperties {
-  public static final String TOMCAT_MAJOR_VERSION = Library.getUniversalProperty('tomcat.major.version', '7')
-  public static final String PUBLIC_IP_ADDRESS = Library.getUniversalProperty('tomcat.public.ip.address', DefaultProperties.HOST)
+  public static final String TOMCAT_MAJOR_VERSION
+  public static final String PUBLIC_IP_ADDRESS
+
+  static {
+    TOMCAT_MAJOR_VERSION = Library.getUniversalProperty('tomcat.major.version')
+    assert TOMCAT_MAJOR_VERSION?.trim() : "TOMCAT_MAJOR_VERSION is either null or empty"
+
+    PUBLIC_IP_ADDRESS = Library.getUniversalProperty('tomcat.public.ip.address', DefaultProperties.HOST)
+  }
 }
