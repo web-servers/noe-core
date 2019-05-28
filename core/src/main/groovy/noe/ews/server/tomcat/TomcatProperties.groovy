172de09223196562e56259955730f0bc036cf81b
Paul <PaulLodge@users.noreply.github.com>
2019-05-28 09:54:03 +0200

    Issues 2 note 78734 correction (#75)
    * Corrected bug in which EAP was not taken into account

--- core/src/main/groovy/noe/ews/server/tomcat/TomcatProperties.groovy
+++ core/src/main/groovy/noe/ews/server/tomcat/TomcatProperties.groovy
@@ -11,13 +11,21 @@ import noe.common.utils.Library
  *
  */
 class TomcatProperties {
+  private static final String EWS_VERSION
   public static final String TOMCAT_MAJOR_VERSION
   public static final String PUBLIC_IP_ADDRESS
 
   static {
+
+    EWS_VERSION = noe.common.DefaultProperties.ewsVersion()
     TOMCAT_MAJOR_VERSION = Library.getUniversalProperty('tomcat.major.version')
-    if (!TOMCAT_MAJOR_VERSION?.trim()) {
-      throw new IllegalArgumentException("TOMCAT_MAJOR_VERSION is either null or empty")
+
+    if (!EWS_VERSION?.trim() && !TOMCAT_MAJOR_VERSION?.trim()) {
+      throw new IllegalArgumentException("Both EWS_VERSION and TOMCAT_MAJOR_VERSION are either null or empty")
+    }
+
+    if (EWS_VERSION?.trim() && !TOMCAT_MAJOR_VERSION?.trim()) {
+      throw new IllegalArgumentException("EWS_VERSION is set but the TOMCAT_MAJOR_VERSION is either null or empty")
     }
 
     PUBLIC_IP_ADDRESS = Library.getUniversalProperty('tomcat.public.ip.address', DefaultProperties.HOST)
