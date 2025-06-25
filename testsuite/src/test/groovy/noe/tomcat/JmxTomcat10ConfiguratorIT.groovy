8b86fbb3c1f5066c1609c20554e7d2e17f9dceb3
Giannis Christodoulou <gchristo@redhat.com>
2025-06-25 13:57:57 +0200

    update-rhel-httpd-tomcat: Adds compatibility restriction for JWS 6.0 and RHEL7

--- testsuite/src/test/groovy/noe/tomcat/JmxTomcat10ConfiguratorIT.groovy
+++ testsuite/src/test/groovy/noe/tomcat/JmxTomcat10ConfiguratorIT.groovy
@@ -15,6 +15,7 @@ class JmxTomcat10ConfiguratorIT extends JmxTomcatConfiguratorIT {
     def serverJavaHomeMatches = java11Indicators.any { DefaultProperties.SERVER_JAVA_HOME?.contains(it) }
 
     Assume.assumeFalse("JWS is not supported on HP-UX => skipping", platform.isHP())
+    Assume.assumeFalse("JWS 6.0 is officially supported on RHEL versions 8 and above", platform.OSVersionLessThan(8));
     Assume.assumeTrue("Tomcat from JWS 6.0 requires at least Java 11",
             serverJavaHomeMatches ?: Java.isJdkXOrHigher('11')
     )
