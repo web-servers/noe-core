b015c48ec7ec0ed94f36143761d79e52fafe6e4d
Giannis Christodoulou <gchristo@redhat.com>
2025-07-08 07:57:17 +0200

    rhel-windows-improvements: Adds test skip rules for different RHEL versions

--- testsuite/src/test/groovy/noe/tomcat/JvmRouteTomcat10ConfiguratorIT.groovy
+++ testsuite/src/test/groovy/noe/tomcat/JvmRouteTomcat10ConfiguratorIT.groovy
@@ -14,7 +14,10 @@ class JvmRouteTomcat10ConfiguratorIT extends JvmRouteTomcatConfiguratorIT {
         def serverJavaHomeMatches = Java.JAVA_11_INDICATORS.any { DefaultProperties.SERVER_JAVA_HOME?.contains(it) }
 
         Assume.assumeFalse("JWS is not supported on HP-UX => skipping", platform.isHP())
-        Assume.assumeFalse("JWS 6.0 is officially supported on RHEL versions 8 and above", platform.OSVersionLessThan(8));
+        if (platform.isRHEL()) {
+            Assume.assumeFalse("JWS 6.0 is officially supported on RHEL versions 8 and above",
+                    platform.OSVersionLessThan(8));
+        }
         Assume.assumeTrue("Tomcat from JWS 6.0 requires at least Java 11",
                 serverJavaHomeMatches ?: Java.isJdkXOrHigher('11')
         )
