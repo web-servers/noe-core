928cb149c5dbc13a46eee7a54559afe3f3d06331
Giannis Christodoulou <gchristo@redhat.com>
2025-07-09 13:46:16 +0200

    rhel-windows-improvements: jws-5.x is not compatible to rhel10

--- testsuite/src/test/groovy/noe/tomcat/BindingsTomcat9ConfiguratorIT.groovy
+++ testsuite/src/test/groovy/noe/tomcat/BindingsTomcat9ConfiguratorIT.groovy
@@ -15,7 +15,10 @@ class BindingsTomcat9ConfiguratorIT extends BindingsTomcatConfiguratorIT {
     Assume.assumeTrue("Tomcat from JWS 5.0 requires at least Java 1.8",
             (DefaultProperties.SERVER_JAVA_HOME?.contains('jdk1.8')) ?: Java.isJdkXOrHigher('1.8')
     )
-
+    if (platform.isRHEL()) {
+      Assume.assumeTrue("JWS 5.0 is officially supported on RHEL versions 9 and lower",
+              platform.OSVersionLessThan(10));
+    }
     loadTestProperties("/tomcat9-common-test.properties")
     prepareWorkspace()
   }
